b02b8d35306a5a34b61ee54eed062c800d545a83
bezdelny <71118329+bezdelny@users.noreply.github.com>
2024-08-11 07:39:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -238,7 +238,7 @@ document.getElementById('startBtn').addEventListener('click', async () => {
 });
 
 document.getElementById('creatorChannelBtn').addEventListener('click', () => {
-    window.location.href = 'https://t.me/pdosi_project';
+    window.location.href = 'https://t.me/goodchain';
 });
 
 function generateClientId() {
